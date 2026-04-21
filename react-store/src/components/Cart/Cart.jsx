f83079497dc57ebdf23718977a64943cee3ade4b
abdalakhras <abdakalakhras@gmail.com>
2026-04-21 21:04:41 +0300

    showing products in card.jsx

--- react-store/src/components/Cart/Cart.jsx
+++ react-store/src/components/Cart/Cart.jsx
@@ -6,8 +6,27 @@ import { CartContext } from "../Context/CartContext";
 function Cart(){
 
     const {cart} = useContext(CartContext)
-
     console.log(cart)
+
+    return(
+        <>
+        <h1>welcome to cart page</h1>
+        <div>
+        {cart?.items?.map((item)=>{
+            return(
+            <ul key={item._id}>
+                <li>{item.productId.name}</li>
+                {/* something wrong with the category  */}
+                <li>{item.productId.category}</li> 
+                 <li>{item.productId.price}</li>
+                   <li>{item.quantity}</li>
+
+            </ul>
+            )
+        })}
+        </div>
+        </>
+    )
 
 }
 
